8e6e201c9122d024061e0f814900dc7f778192f9
Sebastiaan Alvarez Rodriguez van Ravensberg <sebastiaanalva@gmail.com>
2021-03-23 14:20:24 +0100

    Example project complete.

---
+++ app/src/main/cpp/test.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+#include "test.h"
+
+void Test::print() {
+    std::cout << "Hi there, lineno=" << aaa << std::endl;
+}
---
+++ app/src/main/cpp/test.h
@@ -0,0 +1,13 @@
+#ifndef A_TEST_TEST_H
+#define A_TEST_TEST_H
+
+class Test {
+public:
+    void print();
+
+private:
+    int aaa = 42;
+};
+
+
+#endif //A_TEST_TEST_H
